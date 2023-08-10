83772be264611f4f6bb3b11cb7ebe076a022cd47
Cr721432 <danyfalcon3@gmail.com>
2023-08-10 14:02:56 -0600

    codificacion de la clase estudiante

--- Sygesin.EntidadesDeNegocio/src/sygesin/entidadesdenegocio/Estudiante.java
+++ Sygesin.EntidadesDeNegocio/src/sygesin/entidadesdenegocio/Estudiante.java
@@ -1,7 +1,207 @@
 
 package sygesin.entidadesdenegocio;
 
+import java.time.LocalDate;
+
 
 public class Estudiante {
+    
+  private int id;
+    private int rolId;
+    private int seccionId;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private int departamento;
+    private String telefono;
+    private String correo;
+    private String encargado;
+     private String seccion;
+      private String fechanacimiento;
+    private String login;
+    private String password;
+    private byte estatus;
+    private LocalDate fechaRegistro;
+    private int top_aux;
+    private String confirmPassword_aux;
+    private Rol rol;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int id, int rolId, int seccionId, String nombre, String apellido, String direccion, int departamento, String telefono, String correo, String encargado, String seccion, String fechanacimiento, String login, String password, byte estatus, LocalDate fechaRegistro, int top_aux, String confirmPassword_aux, Rol rol) {
+        this.id = id;
+        this.rolId = rolId;
+        this.seccionId = seccionId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.departamento = departamento;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.encargado = encargado;
+        this.seccion = seccion;
+        this.fechanacimiento = fechanacimiento;
+        this.login = login;
+        this.password = password;
+        this.estatus = estatus;
+        this.fechaRegistro = fechaRegistro;
+        this.top_aux = top_aux;
+        this.confirmPassword_aux = confirmPassword_aux;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(int rolId) {
+        this.rolId = rolId;
+    }
+
+    public int getSeccionId() {
+        return seccionId;
+    }
+
+    public void setSeccionId(int seccionId) {
+        this.seccionId = seccionId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(int departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEncargado() {
+        return encargado;
+    }
+
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    public String getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(String fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public String getConfirmPassword_aux() {
+        return confirmPassword_aux;
+    }
+
+    public void setConfirmPassword_aux(String confirmPassword_aux) {
+        this.confirmPassword_aux = confirmPassword_aux;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+    
     
 }
